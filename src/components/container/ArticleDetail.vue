902362b325090bcfc98718ab37e1126a944c1142
znvy <ningvvy@gmail.com>
2019-01-23 15:35:04 +0800

    improvement: chang global css

--- src/components/container/ArticleDetail.vue
+++ src/components/container/ArticleDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row type="flex" class="row-bg" justify="center">
-    <el-col :xs="24" :sm="18" :md="16" :lg="14" :xl="12" class="content">
+    <el-col :xs="24" :sm="18" :md="16" :lg="14" :xl="12">
       <h2>{{title}}</h2>
       <div v-html="content" class="markdown-body" v-highlight></div>
     </el-col>
@@ -37,17 +37,13 @@ export default {
 @import url("../../assets/github-markdown.css");
 
 h2 {
-  text-align: center;
+  margin: 30px auto;
+  text-align: center
 }
 
-.content {
-  padding: 90px 20px 20px;
-}
-
-@media (min-width: 1500px) {
-  .content {
-    max-width: 800px;
-    /* margin: auto */
-  }
+.row-bg {
+  padding: 70px 20px 20px;
+  background-color: #ffffff;
+  min-height: 100%;
 }
 </style>
